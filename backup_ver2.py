393f7294fbb62efe0dc3b10ff5cadf4c7440a702
XINK3R <vladislav.k0707@gmail.com>
2021-02-09 10:10:22 +0300

    Моя первая программа для бекапа

---
+++ backup_ver2.py
@@ -0,0 +1,34 @@
+import os
+import time
+# 1. Файлы и каталоги, которые необходимо скопировать, собираются в список.
+source = ['"C:\\Users\\pcadmin\\Desktop\\Python"', 'C:\\Users\\pcadmin\\Pictures']
+# Заметьте, что для имён, содержащих пробелы, необходимо использовать
+# двойные кавычки внутри строки.
+# 2. Резервные копии должны храниться в основном каталоге резерва.
+target_dir = 'D:\\Backup' # Подставьте ваш путь.
+# 3. Файлы помещаются в zip-архив.
+# 4. Текущая дата служит именем подкаталога в основном каталоге
+today = target_dir + os.sep + time.strftime('%Y-%m-%d')
+# Текущее время служит именем zip-архива
+now = time.strftime('%H-%M-%S')
+# Создаём каталог, если его ещё нет
+if not os.path.exists(today):
+    os.mkdir(today) # создание каталога
+    print('Каталог успешно создан', today)
+# Имя zip-файла
+target = today + os.sep + now + '.zip'
+# 5. Используем команду "zip" для помещения файлов в zip-архив
+zip_command = "C:\PROGRA~1\WinRAR\WinRAR.exe u -as -dh {0} {1}".format(target, ' '.join(source))
+# Запускаем создание резервной копии
+if os.system(zip_command) == 0:
+    print('Резервная копия успешно создана в', target)
+else:
+    print('Создание резервной копии НЕ УДАЛОСЬ')
+
+
+
+
+
+#    rar = "C:\PROGRA~1\WinRAR\WinRAR.exe u -as -dh {0} {1}".format(dst, ' '.join(source))
+''' format - подставляет аргументы {0}=dst, {1}=source
+Метод join преобразовывает список в строку '''
